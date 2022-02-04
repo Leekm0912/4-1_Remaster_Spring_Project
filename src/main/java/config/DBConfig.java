9aba1cf5586d87d4731040d5e225f7c4a35c6c07
Leekm0912 <42348176+Leekm0912@users.noreply.github.com>
2022-02-04 14:35:37 +0900

    DB 설정 프로필 사용

--- src/main/java/config/DBConfig.java
+++ src/main/java/config/DBConfig.java
@@ -2,8 +2,10 @@ package config;
 
 import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -16,28 +18,72 @@ public class DBConfig {
 	@Autowired
 	DataSource ds;
 	
-	@Bean(destroyMethod = "close")
-	public DataSource dataSource() {
-		DataSource ds = new DataSource();
-		ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
-//		ds.setUrl("jdbc:mysql://localhost:3306/projectdb?serverTimezone=UTC&characterEncoding=utf8");
-//		ds.setUsername("leekm");
-		ds.setUrl("jdbc:mysql://localhost:3306/mybatis?serverTimezone=UTC&characterEncoding=utf8");
-		ds.setUsername("mybatis_test");
-		ds.setPassword("1234");
+	@Configuration
+	@Profile("home")
+	public static class HomeDataSourceConfig {
+		@Value("${db.driver}")
+		private String driver;
+		@Value("${db.url}")
+		private String url;
+		@Value("${db.url.args}")
+		private String url_args;
+		@Value("${db.home.schema}")
+		private String schema;
+		@Value("${db.home.id}")
+		private String id;
+		@Value("${db.home.pw}")
+		private String pw;
+		@Bean(destroyMethod = "close")
+		public DataSource dataSource() {
+			DataSource ds = new DataSource();
+			ds.setDriverClassName(driver);
+			ds.setUrl(url+schema+url_args);
+			ds.setUsername(id);
+			ds.setPassword(pw);		
+			ds.setInitialSize(2);
+			ds.setMaxActive(10);
+			ds.setTestWhileIdle(true);
+			ds.setMinEvictableIdleTimeMillis(60000 * 3);
+			ds.setTimeBetweenEvictionRunsMillis(10 * 1000);
+			return ds;
+		}
+	}
+	
+	@Configuration
+	@Profile("school")
+	public class DBSchoolConfig {
+		@Value("${db.driver}")
+		private String driver;
+		@Value("${db.url}")
+		private String url;
+		@Value("${db.url.args}")
+		private String url_args;
+		@Value("${db.school.schema}")
+		private String schema;
+		@Value("${db.school.id}")
+		private String id;
+//		@Value("${db.school.pw}")
+//		private String pw;
 		
-		ds.setInitialSize(2);
-		ds.setMaxActive(10);
-		ds.setTestWhileIdle(true);
-		ds.setMinEvictableIdleTimeMillis(60000 * 3);
-		ds.setTimeBetweenEvictionRunsMillis(10 * 1000);
-		return ds;
+		@Bean(destroyMethod = "close")
+		public DataSource dataSource() {
+			DataSource ds = new DataSource();
+			ds.setDriverClassName(driver);
+			ds.setUrl(url+schema+url_args);
+			ds.setUsername(id);
+			ds.setInitialSize(2);
+			ds.setMaxActive(10);
+			ds.setTestWhileIdle(true);
+			ds.setMinEvictableIdleTimeMillis(60000 * 3);
+			ds.setTimeBetweenEvictionRunsMillis(10 * 1000);
+			return ds;
+		}
 	}
 
 	@Bean
 	public PlatformTransactionManager transactionManager() {
 		DataSourceTransactionManager tm = new DataSourceTransactionManager();
-		tm.setDataSource(dataSource());
+		tm.setDataSource(ds);
 		return tm;
 	}
 	
